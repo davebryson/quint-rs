8025ec0d0e2c6ddc5904310bb3791981e468d1cf
Dave Bryson <davebryson@users.noreply.github.com>
2025-08-14 20:48:14 -0500

    some builtin expr to grammar

--- parser/tests/integration.rs
+++ parser/tests/integration.rs
@@ -1,6 +1,24 @@
 use quint_evaluator::{evaluator::run, ir::LookupTable, value::Value};
 use quint_parser::{QuintError, parse_quint_expr};
 
+fn print_result(expr: &str) {
+    let parsed = parse_quint_expr(expr);
+    assert!(parsed.is_ok());
+    let table = LookupTable::default();
+    let val = run(&table, &parsed.unwrap());
+    assert!(val.is_ok());
+    println!("{:?}", val.unwrap())
+}
+
+fn with_value(expr: &str) -> Result<Value, QuintError> {
+    let parsed = parse_quint_expr(expr);
+    assert!(parsed.is_ok());
+    let table = LookupTable::default();
+    let val = run(&table, &parsed.unwrap());
+    assert!(val.is_ok());
+    val
+}
+
 fn eval_expr(expr: &str) -> Result<Value, QuintError> {
     let parsed = parse_quint_expr(expr);
     assert!(parsed.is_ok());
@@ -51,5 +69,15 @@ fn basic_integration() {
     check_expr!("true implies true", true, bool);
     check_expr!("true implies false", false, bool);
 
+    // if then else
+    check_expr!("if (1==1) true else false", true, bool);
+    check_expr!("if (false) 0 else 10", 10i64, i64);
+
     check_expr!("\"hello\"", "hello", &str);
+}
+
+#[test]
+fn check_list() {
+    let r = with_value("[1,2,3]").unwrap();
+    assert!(r.cardinality() == 3);
 }
